0f102a37da6fae9c4b6fedfce05a3a6a1dc06f7a
quanglv <quangle1410@gmail.com>
2020-12-13 09:23:05 +0700

    quanglv - commit

--- src/main/java/com/quanglv/config/WebSecurityConfig.java
+++ src/main/java/com/quanglv/config/WebSecurityConfig.java
@@ -4,6 +4,7 @@ import com.quanglv.config.oauth2.UsersOauthService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
